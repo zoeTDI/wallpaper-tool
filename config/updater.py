4d6882aaf1272a6172775835e10b326a02a8c171
Caldm <1832400547@qq.com>
2026-06-14 11:20:16 +0800

    feat: 更新方法

---
+++ config/updater.py
@@ -0,0 +1,29 @@
+import json
+
+from utils.isDef import is_valid
+from .loader import get_project_path
+from .schemas import PathConfig
+
+
+def update_config_file(new_path_config: PathConfig):
+    """
+    更新 config.json：直接使用传入的 PathConfig 对象覆盖 json 文件中的对应属性。
+    """
+    config_path = get_project_path() / "config.json"
+
+    if config_path.exists():
+        with open(config_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+    else:
+        data = {"path_config": {"scan_path": None, "out_path": None}}
+    if "path_config" not in data:
+        data["path_config"] = {"scan_path": None, "out_path": None}
+
+    if is_valid(new_path_config.scan_path):
+        data["path_config"]["scan_path"] = new_path_config.scan_path
+
+    if is_valid(new_path_config.out_path):
+        data["path_config"]["out_path"] = new_path_config.out_path
+
+    with open(config_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
