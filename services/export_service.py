127977dfda50fbb20e29a04f794b465ca555395e
Caldm <1832400547@qq.com>
2026-06-14 11:20:42 +0800

    refactor: 更改导出路径的获取

--- services/export_service.py
+++ services/export_service.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 
+from config import app_config
+from config.schemas import PathConfig
+from config.updater import update_config_file
 from utils.files import (
     sanitize_filename,
     limit_filename_length,
@@ -22,15 +25,27 @@ class ExportService:
         full_path.mkdir(parents=True, exist_ok=True)
         return str(full_path)
 
-    def export(self, title: str, source_path: str) -> str:
+    def export(
+        self, title: str, source_path: str, out_path: str = app_config.path.out_path
+    ) -> str:
         if not os.path.exists(source_path):
             raise FileNotFoundError(f"找不到文件: {source_path}")
-        download_dir = get_downloads_folder()
-        target_dir = self._build_target_dir(download_dir, "wallpaper")
+        # 存在自定义输出路径时，采用自定义输出路径
+        if out_path is not None and Path(out_path).is_dir():
+            if not Path(out_path).exists():
+                try:
+                    Path(out_path).mkdir(parents=True, exist_ok=True)
+                except FileExistsError:
+                    out_path = self._build_target_dir(
+                        get_downloads_folder(), "wallpaper"
+                    )
+        else:
+            out_path = self._build_target_dir(get_downloads_folder(), "wallpaper")
 
         safe_title = sanitize_filename(title)
         safe_title = limit_filename_length(safe_title)
 
-        copy_file_path = copy_file_to_folder(source_path, target_dir)
+        copy_file_path = copy_file_to_folder(source_path, out_path)
         rename_file_keep_ext(copy_file_path, safe_title)
-        return target_dir
+        update_config_file(PathConfig(out_path=out_path))
+        return out_path
